27b0b6660101fd5b67efaa6b9d1421f3e6d54e9e
xiaoyangst <2531560753@qq.com>
2024-11-28 15:40:38 +0800

    :bug: fix(AsyncThreadPool): 修复判断逻辑
    修复 worker 中 if 的错误判断逻辑

--- AsyncThreadPool/AsyncThreadPool.cpp
+++ AsyncThreadPool/AsyncThreadPool.cpp
@@ -25,9 +25,9 @@ void AsyncThreadPool::worker() {
   while (!m_stop.load()) {
     taskCallBack task;
     std::unique_lock<std::mutex> lock(m_queueMtx);
-    while (m_taskQueue.empty() && !m_stop) { // 不用 lambda 那种是方便直接 return 关闭线程
+    while (m_taskQueue.empty() && !m_stop.load()) { // 不用 lambda 那种是方便直接 return 关闭线程
       m_cond.wait(lock);
-      if (m_exitThreadNum.load()) {
+      if (m_exitThreadNum.load() > 0) {
         m_exitThreadNum.fetch_sub(1);
         m_curThreadNum.fetch_sub(1);
         m_idleThreadNum.fetch_sub(1);
@@ -64,7 +64,7 @@ void AsyncThreadPool::manager() {
       std::thread::id id = th.get_id();
       m_threadPoolMap[id] = std::move(th);
     }
-      // 什么时候减少线程？有空闲线程且为已有的线程数量的一半，但要让任务线程数量大于最小线程数量，那么就可以减少
+    // 什么时候减少线程？有空闲线程且为已有的线程数量的一半，但要让任务线程数量大于最小线程数量，那么就可以减少
     else if (idleThreadNum > (curThreadNum / 2) && curThreadNum > m_min) {
       int exit_num = m_idleThreadNum.load();
       int result = std::min(exit_num, ASYNC_EXIT_THREAD_NUM);  // 确保销毁的线程小于等于空闲线程的数量
